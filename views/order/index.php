a8401157a02c760eb3044859f3757f9b90e8d49c
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-08 00:01:47 +0100

    clean order/index

--- views/order/index.php
+++ views/order/index.php
@@ -131,60 +131,10 @@
 		<a href="/order/BladeVPS/4"><button type="button" class="btn btn-primary bladeVPSindex_btn">Order</button></a>
 	</div>
 
-<<<<<<< HEAD
-</div>
 
-=======
-				<div class="col-sm-3 combox">
-					<h4>+3</h4>
-				</div>
-				
-				<div class="col-sm-3 combox">
-					<h4>+4</h4>
-				</div>
-				
-				<div class="col-sm-3 combox">
-					<h4>+5</h4>
-				</div>
-				
-				<div class="col-sm-3 combox">
-					<h4>+6</h4>
-				</div>
-			</div>
-			<br>
-			<hr>
-			<p class="lead">Memory</p>
+	</div>
+</div><!--eind col-md-10-->
 			
-			<br>
-			<hr>
-			<p class="lead">Drives</p>
-			
-			<br>
-			<hr>
-			<button class="btn btn-lg btn-primary btn-block" type="submit">Go to Step 2</button>
-        </div>
-        <div class="col-lg-3">
-          <h2>Your VPS</h2>
-          <p></p>
-          <p><a class="btn btn-primary" href="#" role="button">Buy &raquo;</a></p>
-       </div>
-    </div>
 
-	<br>
-	<hr>
-	
-	<br><br><br>
-	
-	<div class="bs-example">
-						<p class="text-muted">Fusce dapibus, tellus ac cursus commodo, tortor mauris nibh.</p>
-						<p class="text-primary">Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-						<p class="text-success">Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-						<p class="text-info">Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-						<p class="text-warning">Etiam porta sem malesuada magna mollis euismod.</p>
-						<p class="text-danger">Donec ullamcorper nulla non metus auctor fringilla.</p>
-					</div>
->>>>>>> f69c88650f37b26bcb183a986b6ff97c005343b8
-
-</div>
 <div class="col-md-1"></div>
 </div>
