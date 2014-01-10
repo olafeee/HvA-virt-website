d547c0f7cae7c88ea74bf94b275e8519de9c790d
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-10 19:41:07 +0100

    Login test

--- models/loginModel.php
+++ models/loginModel.php
@@ -15,16 +15,11 @@ class loginModel extends baseModel
 	public function run()
 	{
 		$username = $_POST['login'];
-		echo"ik ben een hondelul";
-		$sth = $this->db->prepare("SELECT id FROM users WHERE 
-				login = :login AND password = :password");
-		$sth->execute(array(
-			':login' => $_POST['login'],
-			':password' => $_POST['password']
-		));
+		$password = $_POST['password'];
+		$sth = $cloudstack->login($username, $password);
 		
 		$data = $sth->fetchAll();
-		//print_r($data);
+		print_r($data);
 
 		
 		$count =  $sth->rowCount();
