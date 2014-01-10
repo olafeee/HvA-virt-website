a5b3141c4bee088c46d366bb227ec9e2b7c0b536
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-10 20:49:19 +0100

    edit login page

--- models/loginModel.php
+++ models/loginModel.php
@@ -20,32 +20,11 @@ class loginModel extends baseModel
 	public function run()
 	{
 		$username = $_POST['login'];
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 		$password = $_POST['password'];
 		$sth = $cloudstack->login($username, $password);
-=======
-<<<<<<< HEAD
->>>>>>> b44e7cfe4087fc185bdcda1299064d1a4424ed25
-		$sth = $this->db->prepare("SELECT id FROM users WHERE 
-				login = :login AND password = :password");
-		$sth->execute(array(
-			':login' => $_POST['login'],
-			':password' => $_POST['password']
-		));
-<<<<<<< HEAD
-=======
-=======
-		$password = $_POST['password'];
-		$sth = $cloudstack->login($username, $password);
->>>>>>> pr/3
->>>>>>> 05e920b2ffa2917fa89bd0ebf12541c480994b78
->>>>>>> b44e7cfe4087fc185bdcda1299064d1a4424ed25
-		
+
 		$data = $sth->fetchAll();
 		//print_r($data);
-
 		
 		$count =  $sth->rowCount();
 		if ($count > 0) {
