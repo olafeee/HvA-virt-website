46c464c0183f7b3c7f9b1c0d0a8ec1a02a27726a
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-10 19:13:17 +0100

    Attempt to start the account login process

--- controllers/klantPaneel.php
+++ controllers/klantPaneel.php
@@ -1,5 +1,7 @@
 <?php
 
+//require_once("lib/cloudstack.php");
+
 class klantPaneel extends baseController {
 
 	function __construct() {
--- controllers/login.php
+++ controllers/login.php
@@ -1,6 +1,10 @@
 <?php
 
+require_once("lib/cloudstack.php");
+
 class Login extends baseController {
+
+	public cloudstack = new cloudstack();
 
 	function __construct() {
 		parent::__construct();	
@@ -10,4 +14,12 @@ class Login extends baseController {
 		$model = $this->loadModel();
 		$model->run();
 	}//eind run
+
+	function login($username, $password){
+		$cloudstack->login($username, $password);
+	}
+
+	function logout(){
+		$cloudstack->logout();
+	}
 }//eind class
