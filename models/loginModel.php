184cd1b96e525808615e15704f75fda1a02ba1d3
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-13 00:23:51 +0100

    loginModel verbetering en /login/login probleem update

--- models/loginModel.php
+++ models/loginModel.php
@@ -19,36 +19,20 @@ class loginModel extends baseModel
 
 	public function run()
 	{
-		echo"ik doe het wel maar ook niet";
 		$username = $_POST['login'];
 		$password = $_POST['password'];
 		$sth = $this->cloudstack->login($username, $password);
 		$data = json_decode($sth,true);
 
-
-			/*  test scirpt	
-		echo "<br/> <pre>";
-		print_r($data);
-		echo "<br/>";
-		var_dump(json_decode($sth));
-		echo $users;
-		echo $data['loginresponse'];
-		echo "<br/> </pre>";
-		*/
-
 		if (is_array($data) && array_key_exists("loginresponse", $data)) {
-			// login
 			Session::init();
 			Session::set('loggedIn', true);
 			foreach ($data['loginresponse'] as $k => $v) {
 		    	Session::set($k, $v);
 		    }
-			
 			header('location: ../account');
 		} else {
-			echo "<br/>";
-			echo("else vaal");
-			//header('location: ../login');
+			header('location: ../login');
 		}
 		
 	}
