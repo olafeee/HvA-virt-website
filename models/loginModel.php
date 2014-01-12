f75ca4c0647de5c6fd93ccee0717e54698d5ba11
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-12 17:34:47 +0100

    fds

--- models/loginModel.php
+++ models/loginModel.php
@@ -25,8 +25,8 @@ class loginModel extends baseModel
 		$sth = $this->cloudstack->login($username, $password);
 		echo "<br/>";
 		print_r($sth);
-		echo "<br/>";
-		 echo("status = {$sth->loginresponse}");
+		$obj = json_decode($sth);
+		print $obj->{'loginresponse'}; // 12345
 		echo "<br/>";
 
 
