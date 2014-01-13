c57f755fcf6aa4bf5048b83d9d2a682e785b4be1
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-13 20:49:05 +0100

    json array login verbeterd

--- models/loginModel.php
+++ models/loginModel.php
@@ -23,20 +23,16 @@ class loginModel extends baseModel
 		$password = $_POST['password'];
 		$sth = $this->cloudstack->login($username, $password);
 		$data = json_decode($sth,true);
-		$test = $data['loginresponse'];
-		echo"<pre>";
-		print_r($test['username']);
 
-		/*if (is_array($data) && array_key_exists("loginresponse", $data)) {
+		if (is_array($data) && array_key_exists("loginresponse", $data)) {
+			$loginArray = $data['loginresponse'];
 			Session::init();
 			Session::set('loggedIn', true);
-			foreach ($data['loginresponse'] as $k => $v) {
-		    	Session::set($k, $v);
-		    }
-			//header('location: ../account');
+		    Session::set('logArr', $loginArray);
+			header('location: ../account');
 		} else {
 			header('location: ../login');
-		}*/
+		}
 		
 	}
 }
