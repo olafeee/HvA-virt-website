7b5f001b4290e42699c34400d314cab2cbb090d8
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-13 14:04:41 +0100

    merge error

--- views/login/index.php
+++ views/login/index.php
@@ -1,18 +1,26 @@
 <div class="row">
-	<div class="col-md-3"></div>
-	<div class="col-md-6">
-		<div class="login-class">
-			<img src="/img/plaintech-logo.png" id="img-login-logo">
-      <form class="form-signin" action="../login/run" method="post">
-        <input type="text" class="form-control" placeholder="Email address" required="" autofocus="" name="login">
-        <input type="password" class="form-control" placeholder="Password" required="" name="password">
-        <label>
-          <a href="register">Maak een account aan</a>
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
-		</div>
-	</div>
-	<div class="col-md-3"></div>
-</div>
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+                <div class="login-class">
+                        <img src="/img/plaintech-logo.png" id="img-login-logo">
+                        
 
+
+
+      <form class="form-signin" action="../login/run" method="post">
+            <input type="text" class="form-control" placeholder="Email address" required="" autofocus="" name="login">
+            <input type="password" class="form-control" placeholder="Password" required="" name="password">
+            <label class="checkbox">
+              <input type="checkbox" value="remember-me"> Onthoud mij
+            </label>
+            <label>
+              <a href="register">Maak een account aan</a>
+        </label>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+
+                </div>
+        </div>
+        <div class="col-md-3"></div>
+</div>
