6c64be48b314a96f749704c04ce0163864db479d
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-13 22:02:33 +0100

    test #nein of #neun

--- views/klantPaneel/index.php
+++ views/klantPaneel/index.php
@@ -75,6 +75,16 @@ if (isset($_SESSION['loggedIn'])):?>
             <td class="IPAdres"><?php echo $vmResponce[$xy]["nic"][0]["ipaddress"];?></td>
             <td><button type="button" class="btn btn-info"> Info </button> <button type="button" class="btn btn-success"> Upgrade </button></td>
           </tr>
+          <tr class="success">
+            <td class"displayname"><?php echo $vmResponce[$xy]["displayname"];?></td>
+            <td class="status"><?php echo $vmResponce[$xy]["state"];?></td>
+            <td class="CPU"><?php echo $vmResponce[$xy]["cpunumber"];?></td>
+            <td class="CPUSPEED"><?php echo $vmResponce[$xy]["cpuspeed"];?></td>
+            <td class="memory"><?php echo $vmResponce[$xy]["memory"];?></td>
+            <td class="HHD"><?php echo $vmResponce[$xy]["displayname"];?></td>
+            <td class="IPAdres"><?php echo $vmResponce[$xy]["nic"][0]["ipaddress"];?></td>
+            <td><button type="button" class="btn btn-info"> Info </button> <button type="button" class="btn btn-success"> Upgrade </button></td>
+          </tr>
 
         </tbody>
       </table>
