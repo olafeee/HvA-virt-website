d01423e48ed1f8f549f9ec6a1b1cf5180372d47d
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-14 10:34:21 +0100

    commit

--- views/klantPaneel/VM.php
+++ views/klantPaneel/VM.php
@@ -1,6 +1,20 @@
 <?php if (isset($_SESSION['loggedIn'])):
 $vmResponce = $this->vmResponce;
 $vmNumber = $this->vmNumber;
+  // maak van subnet een prifix
+  function prefixSubnet($input){
+    $subBin = explode( '.', $input );
+    $subBinX = 0;
+    $subnet = 0;
+
+    while ($subBinX <= 3) {
+      $x = decbin($subBin[$subBinX]);
+      $var3 = strlen(str_replace('0', '', $x));
+      $subnet = $subnet + $var3;
+      $subBinX++;
+    }
+    return $subnet;
+  }
 ?>
 
 <div class="row">
@@ -8,33 +22,36 @@ $vmNumber = $this->vmNumber;
 <?php include $_SERVER['DOCUMENT_ROOT'].'/views/klantPaneel/klantpaneelHeader.php'; ?>
 </div>
 <div class="col-md-8">
-  <table class="table">
-        <thead>
-          <tr>
-            <th>VM</th>
-            <th>Status</th>
-            <th>CPU</th>
-            <th>CPU speed</th>
-            <th>Memory</th>
-            <th>HDD</th>
-            <th>IP Adres</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="success">
-            <td>1</td>
-            <td class="status"><?php echo $vmResponce[$vmNumber]["displayname"];?></td>
-            <td class="CPU"></td>
-            <td class="CPUSPEED"></td>
-            <td class="memory"></td>
-            <td class="HHD"></td>
-            <td class="IPAdres"></td>
-            <td><button type="button" class="btn btn-info"> Info </button> <button type="button" class="btn btn-success"> Upgrade </button></td>
-          </tr>
-
-        </tbody>
-      </table>
+<table class="table">
+  <tr>
+    <td>VM naam</td>
+    <td><?php echo $vmResponce[$vmNumber]["displayname"];?></td>
+  </tr>
+  <tr>
+    <td>State:</td>
+    <td><?php echo $vmResponce[$vmNumber]["state"];?></td>
+  </tr>
+  <tr>
+    <td>CPU cores:</td>
+    <td><?php echo $vmResponce[$vmNumber]["cpunumber"];?>/td>
+  </tr>
+  <tr>
+    <td>CPU speed:</td>
+    <td><?php echo $vmResponce[$vmNumber]["cpuspeed"];?> MHz</td>
+  </tr>
+  <tr>
+    <td>Memory:</td>
+    <td><?php echo $vmResponce[$vmNumber]["memory"];?> MB</td>
+  </tr>
+  <tr>
+    <td>Harddisk:</td>
+    <td>20 GB</td>
+  </tr>
+  <tr>
+    <td>IP addres</td>
+    <td><?php echo $vmResponce[$xy]["nic"][0]["ipaddress"]." /". prefixSubnet($vmResponce[$xy]["nic"][0]["netmask"]);?></td>
+  </tr>
+</table>
     </div>
 <?php else: 
 header('location: ../login');
