2140f241ad5572f84956015b25f708491018dafe
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-14 18:17:10 +0100

    x

--- views/klantPaneel/index.php
+++ views/klantPaneel/index.php
@@ -64,7 +64,7 @@ $vmResponce = $this->vmResponce;
       </table>
 
 <div class="progress progress-striped">
-  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $vmResponce[$xy]["cpuused"];?>%">
     <span class="sr-only">40% Complete (success)</span>
   </div>
 </div>
