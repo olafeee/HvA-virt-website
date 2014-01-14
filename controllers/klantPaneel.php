e136f955d7a9ab1f5e6ee7e1f47972cd5d84676a
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-14 09:44:34 +0100

    hope

--- controllers/klantPaneel.php
+++ controllers/klantPaneel.php
@@ -29,8 +29,7 @@ class klantPaneel extends baseController {
 	}
 
 	function VM(){
-		$model = $this->laadModel();
-		$this->baseView->vmResponce = $model->getVM();
+		$this->baseView->vmResponce = $this->model->getVM();
 		$this->baseView->vmNumber = $value;
 		$this->index('VM');	
 	}
