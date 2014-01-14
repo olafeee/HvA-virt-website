3e840af1fd2f2fc5ceed89f72144a078c1699f83
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-14 09:28:52 +0100

    commit

--- views/klantPaneel/klantpaneelHeader.php
+++ views/klantPaneel/klantpaneelHeader.php
@@ -1,6 +1,6 @@
 
 <ul class="nav nav-tabs nav-stacked kp_div_right">
   <li><a href="/klantPaneel">Klant paneel</a></li>
-  <li><a href="/klantPaneel/Console">Consoles</a></li>
+  <li><a href="/klantPaneel/console">Consoles</a></li>
   <li><a href="/klantPaneel/os_installatie">OS installatie</a></li>
 </ul>
