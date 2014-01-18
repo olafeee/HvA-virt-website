d5516df24ff5d75188676f09a7520dc2031ac363
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-19 00:13:15 +0100

    test

--- models/indexModel.php
+++ models/indexModel.php
@@ -12,24 +12,19 @@ class indexModel extends baseModel
 
 	function getIndex(){
 		$page = "1";
-				$sql = $this->db->prepare("SELECT *
-										FROM CMS_website
-										WHERE page = $page");
-			    $st = $this->$db->prepare( $sql );
-			    $st->bindValue( ":numRows", $numRows, PDO::PARAM_INT );
-			    $st->execute();
-			    $list = array();
-			 
-			    while ( $row = $st->fetch() ) {
-			      $article = new Article( $row );
-			      $list[] = $article;
-			    }
-			 
-			    // Now get the total number of articles that matched the criteria
-			    $sql = "SELECT FOUND_ROWS() AS totalRows";
-			    $totalRows = $conn->query( $sql )->fetch();
-			    $conn = null;
-			    return ( array ( "results" => $list, "totalRows" => $totalRows[0] ) );
+				$sql "SELECT * FROM CMS_website WHERE page > :page";
+				//$sql = "SELECT naam FROM tabel WHERE leeftijd > :leeftijd"; 
+			     
+			    $stmt = $this->$db->prepare($sql); 
+			    $stmt->bindParam(':page', $page, PDO::PARAM_INT); 
+			     
+			    $leeftijd = "test"; 
+			    $stmt->execute(); 
+			     
+			    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) 
+			    { 
+			        var_dump($row);
+			    } 
 
 	}
 
