339b075a190510d5d8513c648b8075672b8c8100
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-18 23:59:40 +0100

    te

--- lib/database.php
+++ lib/database.php
@@ -8,7 +8,7 @@ class Database extends PDO
 	
 	public function __construct()
 	{
-		parent::__construct(DB_TYPE.':host='.DB_HOST.';port=3307;dbname='.DB_NAME, DB_USER, DB_PASS);
+		//parent::__construct(DB_TYPE.':host='.DB_HOST.';port=3307;dbname='.DB_NAME, DB_USER, DB_PASS);
 		parent::__construct(DB_TYPE.':host='.DB_HOST1.';port=3307;dbname='.DB_NAME1, DB_USER1, DB_PASS1);
 	}	
 }
