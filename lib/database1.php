6f6f86f1223514a89b72ac4af4d362c9e5e2b7bd
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-19 12:27:37 +0100

    test

--- lib/database1.php
+++ lib/database1.php
@@ -9,7 +9,18 @@ class Database1 extends PDO
 	public function __construct()
 	{
 		parent::__construct(DB_TYPE.':host='.DB_HOST1.';port=3307;dbname='.DB_NAME1, DB_USER1, DB_PASS1);
-	}	
+	}
+	
+	public function select($sql, $array = array(), $fetchMode = PDO::FETCH_ASSOC)
+    {
+        $sth = $this->prepare($sql);
+        foreach ($array as $key => $value) {
+            $sth->bindValue("$key", $value);
+        }
+        
+        $sth->execute();
+        return $sth->fetchAll($fetchMode);
+    }
 }
 
 ?>
