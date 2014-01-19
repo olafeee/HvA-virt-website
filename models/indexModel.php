bc4da39c3c01d8a5fa72b7efc166182ac21c8465
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-19 13:58:38 +0100

    test

--- models/indexModel.php
+++ models/indexModel.php
@@ -6,17 +6,17 @@ class indexModel extends baseModel
 	function __construct()
 	{
 		parent::__construct();
+		$
 	}
 
 	function getIndex(){
-				$test = "test";
+				$test = "index";
 			    $sqlArray = $this->db->select('SELECT * FROM CMS_website WHERE 
 						page = :page', 
                 array('page' => $test));
-                print_r($sqlArray);
                 $i = 0;
 			    while ($i <= count($sqlArray)) {
-			    	$id = $sqlArray[0];
+			    	$id = $sqlArray[$i];
 			    	$arr = array($id['cwid'] => $id['cmstext'] );
 			    	$i++;
 			    }
