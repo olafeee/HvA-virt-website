aa48a4c751950aa52c109d371cdd7aa49c381382
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-19 15:15:09 +0100

    test

--- models/cmsPlaintechModel.php
+++ models/cmsPlaintechModel.php
@@ -8,7 +8,12 @@ class cmsPlaintechModel extends baseModel
 		parent::__construct();
 	}
 
-	function getCmsIndex($pageid){
+	function getCmsIndex(){
+			    $sqlArray = $this->db->select('SELECT page FROM CMS_website';
+                return $sqlArray;
+	}
+
+	function getCmsIndex1($pageid){
 			    $sqlArray = $this->db->select('SELECT * FROM CMS_website WHERE 
 						page = :page', 
                 array('page' => $pageid));
