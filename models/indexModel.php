692f2111c5e4414fe22caec9b22640d8eb2d4d75
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-19 13:25:14 +0100

    test

--- models/indexModel.php
+++ models/indexModel.php
@@ -9,34 +9,17 @@ class indexModel extends baseModel
 	}
 
 	function getIndex(){
-				/*$sql = "SELECT naam FROM tabel WHERE leeftijd > :leeftijd"; 
-			     
-			    //$stmt = $this->$db->prepare($sql); 
-			    $stmt = $this->db->prepare("SELECT text FROM CMS_website WHERE page > :page");
-
-			    $stmt->bindParam(':page', $page, PDO::PARAM_INT); 
-			     
-			    $page = "test"; 
-			    $stmt->execute(); 
-			     
-			    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) 
-			    { 
-			         echo $row['text'].'<br>'; 
-			    } */
-			    $test = "2";
-			    return $this->db->select('', 
-                array('cwid' => $test));
-
-                			    $test = "2";
+				$test = "2";
 			    $sqlArray = $this->db->select('SELECT * FROM CMS_website WHERE 
-						cwid = :cwid', 
-                array('cwid' => $test));
+						page = :cwid', 
+                array('page' => $test));
                 $i = 0;
 			    while ($i <= count($sqlArray)) {
-			    	$swlArray[]=
+			    	$id = $sqlArray[$i]["swid"];
+			    	$i++
 			    }
 
-                return $swlArray;
+                return $i;
 			}
 
 
