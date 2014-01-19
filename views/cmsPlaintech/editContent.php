9c6084c850a7cbdeba89d900fe4acfa168ebd2bf
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-19 18:29:35 +0100

    ttes

--- views/cmsPlaintech/editContent.php
+++ views/cmsPlaintech/editContent.php
@@ -3,7 +3,7 @@ $value = $this->editContenVar;
 print_r($value);
 ?>
 <form name="input" action="/cmsPlaintech/insertContent/" method="post">
-	text: <input type="text" name="cmstext">
-	<input type="text" name="cwid">
+	text: <input type="text" name="cmstext" value="<?php echo $value[0]['cmstext']; ?>">
+	value <input type="text" name="cwid" value="<?php echo $value[0]['cwid']; ?>">
 	<input type="submit" value="Submit">
 </form>
