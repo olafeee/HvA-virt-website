dd1ae1be50c9e7930f5d400207c2882d5240306b
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-19 18:10:51 +0100

    test

--- views/cmsPlaintech/viewPage.php
+++ views/cmsPlaintech/viewPage.php
@@ -2,8 +2,13 @@
 
 	$text = $this->viewpage;
 
-print_r($text)
+	$i = 0;
+	while ($i < count($text)) {
+		echo 'number = '.$text[$i]["cwid"];
+		echo $text[$i]["cmstext"];
+		echo '<a href="/cmsPlaintech/editContent/'.$text[$i]["cwid"].'">X</a>';
+		echo"<br/>";
+		$i++;
+	}
 
 ?>
-
-
