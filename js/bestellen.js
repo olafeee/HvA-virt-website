6c1861205ac82f1a149063d664b079268dd2b552
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-19 23:45:19 +0100

    d

--- js/bestellen.js
+++ js/bestellen.js
@@ -14,7 +14,7 @@ var SLAprice = SLAarray[0][1];
 var CPUamount = CPUarray[standardValueCPU];
 var RAMamount = RAMarray[standardValueRAM];
 alert(standardValueDisk);
-var DISKamount = DISKarr[standardValueDisk]['DiskAmount']);;
+var DISKamount = DISKarr[standardValueDisk]['DiskAmount'];;
 var NTamount = NTarray[standardValueNT];
 var IPv4amount = standardValueIPv4;
 var SLApacket = SLAarray[0];
