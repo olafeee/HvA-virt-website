4cf559e2b9edba2e8e19f33d9ea0dc712643a08a
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-20 19:54:40 +0100

    Form validation

--- views/account/register.php
+++ views/account/register.php
@@ -7,23 +7,23 @@
 	  <br />
 
 	  <div class="form-group">
-	    <label for="email" class="col-sm-2 control-label">Email</label>
+	    <label for="email" class="col-sm-2 control-label">Email:</label>
 	    <div class="col-sm-10">
 	      <input type="email" class="form-control" id="email" placeholder="Email" data-validation="email" data-validation-help="Vailed email needed to login!">
 	    </div>
 	  </div>
 
 	  <div class="form-group">
-	    <label for="password" class="col-sm-2 control-label">Password</label>
+	    <label for="password" class="col-sm-2 control-label">Password:</label>
 	    <div class="col-sm-10">
 	      <input type="password" class="form-control" id="password" placeholder="Password" data-validation="strength" data-validation-strength="1" data-validation-help="Please pick a strong password!">
 	    </div>
 	  </div>
 
 	  <div class="form-group">
-	    <label for="passwordConfirm" class="col-sm-2 control-label">Confirm</label>
+	    <label for="passwordConfirm" class="col-sm-2 control-label">Confirm:</label>
 	    <div class="col-sm-10">
-	      <input type="password" class="form-control" id="passwordConfirm" placeholder="Password" data-validation="confirmation">
+	      <input type="password" class="form-control" id="passwordConfirm" placeholder="Confirm Password" data-validation="confirmation">
 	    </div>
 	  </div>
 
@@ -31,42 +31,42 @@
 	  <br />
 
 	  <div class="form-group">
-	    <label for="fName" class="col-sm-2 control-label">First Name</label>
+	    <label for="fName" class="col-sm-2 control-label">First Name:</label>
 	    <div class="col-sm-10">
 	      <input type="text" class="form-control" id="fName" placeholder="First Name" data-validation="alphanumeric">
 	    </div>
 	  </div>
 
 	  <div class="form-group">
-	    <label for="lName" class="col-sm-2 control-label">Last Name</label>
+	    <label for="lName" class="col-sm-2 control-label">Last Name:</label>
 	    <div class="col-sm-10">
 	      <input type="text" class="form-control" id="lName" placeholder="Last Name" data-validation="alphanumeric">
 	    </div>
 	  </div>
 
 	  <div class="form-group">
-	    <label for="adzip" class="col-sm-2 control-label">Zip Code</label>
+	    <label for="adzip" class="col-sm-2 control-label">Zip Code:</label>
 	    <div class="col-sm-10">
 	      <input type="text" class="form-control" id="adzip" placeholder="Zip Code" data-validation="required">
 	    </div>
 	  </div>
 
 	  <div class="form-group">
-	    <label for="adnr" class="col-sm-2 control-label">Street Number</label>
+	    <label for="adnr" class="col-sm-2 control-label">Street Number:</label>
 	    <div class="col-sm-10">
 	      <input type="text" class="form-control" id="adnr" placeholder="Street Number" data-validation="number" data-validation-allowing="float">
 	    </div>
 	  </div>
 
 	  <div class="form-group">
-	    <label for="country" class="col-sm-2 control-label">Country</label>
+	    <label for="country" class="col-sm-2 control-label">Country:</label>
 	    <div class="col-sm-10">
 	      <select id="country" class="form-control bfh-countries" data-country="NL" data-validation="country"></select>
 	    </div>
 	  </div>
 
 	  <div class="form-group">
-	    <label for="country" class="col-sm-2 control-label">Phone</label>
+	    <label for="country" class="col-sm-2 control-label">Phone:</label>
 	    <div class="col-sm-10">
 	      <input type="tel" class="form-control bfh-phone" id="number" data-country="country" data-validation="length" data-validation-length="min9">
 	    </div>
