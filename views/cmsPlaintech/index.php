514d1825acc9524876c9c68246aa6be3ba2e6401
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-21 00:21:46 +0100

    buttons

--- views/cmsPlaintech/index.php
+++ views/cmsPlaintech/index.php
@@ -10,5 +10,8 @@
 		    	$i++;
 		    }
 		?>
+		<a href="/cmsPlaintech/manangeVpsParts/CPU"><button type="button" class="btn btn-info">CPU</button></a>
+		<a href="/cmsPlaintech/manangeVpsParts/RAM"><button type="button" class="btn btn-info">RAM</button></a>
+		<a href="/cmsPlaintech/manangeVpsParts/Disk"><button type="button" class="btn btn-info">DISK</button></a>
 	</div>
 </div>
