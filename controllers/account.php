338ee470a94740a424b228b52d59f3b67ae064e0
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-20 23:46:35 +0100

    Create Account

--- controllers/account.php
+++ controllers/account.php
@@ -29,7 +29,7 @@ class Account extends baseController {
 	}
 
 	function registerValidate($input) {
-		if ($input == 'email') {
+		if ($input == 'email' && isset($_POST['submit'])) {
 			$model = $this->laadModel();
 			$model->checkUser($_POST['email']);
 		}
@@ -43,7 +43,7 @@ class Account extends baseController {
 			if ($response == true ) {
 				$this->index('register_success');
 			}
-		} else if ($input == 'email' && !isset($_POST['email'])) {
+		} else if ($input == 'email' && isset($_POST['email'])) {
 			$model = $this->laadModel();
 			$model->checkUser($_POST['email']);
 		} else {
