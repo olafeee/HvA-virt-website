aa80ae3a02095469758461b89e04f302bc284563
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-20 17:53:45 +0100

    Form validation

--- views/account/register.php
+++ views/account/register.php
@@ -39,8 +39,7 @@
 					<div class="input-group">
 						<span class="input-group-addon"><span class="glyphicon glyphicon-home"></span></span>
 						<input type="text" class="form-control" id="adzip" placeholder="Zip Code">
-						<input type="number" class="form-control bfh-number" id="adnr" placeholder="Street Number" min="1">
-						<input type="number" class="form-control" >
+						<input type="number" class="form-control bfh-number" data-min="1" id="adnr" placeholder="Street Number">
 						<select id="country" class="form-control bfh-countries" data-country="NL"></select>
 						
 					</div>
