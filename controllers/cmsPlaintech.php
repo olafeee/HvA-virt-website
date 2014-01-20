85fe9f69c57d889e2014decfb20fc7de0dae3329
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-20 23:37:19 +0100

    //

--- controllers/cmsPlaintech.php
+++ controllers/cmsPlaintech.php
@@ -56,7 +56,7 @@ class cmsPlaintech extends baseController {
 		$pageid = $_POST['pageid'];
 		$cmstext = $_POST['cmstext'];
 		$this->model->insertOneCmsItem($cwid, $pageid, $cmstext);
-		header('location: /cmsPlaintech/viewPage/'.$pageid);
+		//header('location: /cmsPlaintech/viewPage/'.$pageid);
 	}
 
 	 /**
