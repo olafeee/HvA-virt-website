11c5d47ab892ee01767544f7f33affdaea1fafdc
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-20 20:50:51 +0100

    Create Account

--- views/account/register.php
+++ views/account/register.php
@@ -74,7 +74,7 @@
 
 	  <div class="form-group">
 	    <label for="reseller" class="col-sm-2 control-label">Reseller :</label>
-	    <div class="col-sm-offset-2 col-sm-10">
+	    <div class="col-sm-10">
 	      <div class="checkbox">
 	        <label>
 	          <input type="checkbox" name="reseller" data-validation-help="Select this if your a reseller.">I would like a reseller's account.</label>
