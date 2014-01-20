314e29f32fc6cf355bccbce5a7c6b49a7adc9708
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-20 13:05:32 +0100

    test

--- views/cmsPlaintech/manangeVpsParts.php
+++ views/cmsPlaintech/manangeVpsParts.php
@@ -5,7 +5,7 @@
 	echo'<form name="input" action="/cmsPlaintech/insertContent/" method="post">';
 	while ($i < count($text)) {
 		echo '<input type="text" name="DiskAmount" value="'.$text[$i]['DiskAmount'].'">';
-		echo '<input type="text" name="DiskValue" value="'.$text[$i]['DiskValue'].'">';		
+		echo '<input type="text" name="DiskValue" value="'.$text[$i]['DiskPrice'].'">';		
 		echo"<br/>";
 		$i++;
 	}
