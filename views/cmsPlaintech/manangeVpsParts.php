54cc53621d9b84e398fe528a026868e92d196985
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-20 13:04:05 +0100

    test

--- views/cmsPlaintech/manangeVpsParts.php
+++ views/cmsPlaintech/manangeVpsParts.php
@@ -1,5 +1,18 @@
 <?php
 	$text = $this->cmsMVP;
-	print_r($text);
 
+	$i = 0;
+	echo'<form name="input" action="/cmsPlaintech/insertContent/" method="post">';
+	while ($i < count($text)) {
+		echo '<input type="text" name="DiskAmount" value="'.$text[$i]['DiskAmount'].'">';
+		echo '<input type="text" name="DiskValue" value="'.$text[$i]['DiskValue'].'">';		
+		echo"<br/>";
+		$i++;
+	}
 ?>
+</form>
+
+
+
+	
+
