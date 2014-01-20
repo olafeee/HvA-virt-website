266d0a6b7a87e93ecc6b0a26bbeecc7e75231bf1
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-20 19:10:25 +0100

    Form validation

--- views/account/register.php
+++ views/account/register.php
@@ -1,7 +1,47 @@
-<div class="row">
+
 	<div class="row">
 		<h2>Login Information</h2>
 		<br />
+
+
+		<form role="form">
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Email address</label>
+		    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputPassword1">Password</label>
+		    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputFile">File input</label>
+		    <input type="file" id="exampleInputFile">
+		    <p class="help-block">Example block-level help text here.</p>
+		  </div>
+		  <div class="checkbox">
+		    <label>
+		      <input type="checkbox"> Check me out
+		    </label>
+		  </div>
+		  <button type="submit" class="btn btn-default">Submit</button>
+		</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
 		<!-- New style test -->
 		<form class="form-horizontal" role="form" action="../account/runRegister" method="POST" autocomplete="on">
 			<div class="form-group">
@@ -97,4 +137,3 @@
 		</script>
 		<br />
 	</div>
-</div>
