202b74f8a48632d7a3deaa9adb6c76b7500ee18d
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-21 13:27:09 +0100

    Console test

---
+++ views/console/index.php
@@ -0,0 +1,31 @@
+ <?php
+
+ $refresh = $this->refresh;
+
+ ?>
+
+    <center>
+      <?php 
+        $console_uuid = '163a66dc-1df6-4c75-b16f-2db34d91d026';
+        $console_vps_name = 'ConsoleTest';
+        $refresh = ;
+        if($refresh == ""){
+            echo "<meta HTTP-EQUIV='REFRESH' content='5; url=/console/refresh'> ";
+        } 
+                                            
+        $link = "http://145.92.14.90:8080/client/console?cmd=access&vm=".$console_uuid."";
+        echo "<iframe frameborder='0' width='640' height='420' src=\"".$link."\"></iframe>";
+      ?> 
+
+      <?php
+         
+        $user = "admin";
+        $cmdline = "command=login&username=admin&password=R_47*Qp12&response=json";
+        $cmdline = str_replace(" ", "%20", $cmdline);
+        $url = "http://145.92.14.90:8080/client/api?".$cmdline;
+                
+        echo "<iframe  style='display:none;' src='".$url."'></iframe>
+        <meta url=/console />";
+      ?>
+
+    </center> 
