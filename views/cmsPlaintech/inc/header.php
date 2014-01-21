65cc7c0c661346f7d8a66560975d19894a318058
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-21 20:37:50 +0100

    A CHANGE

--- views/cmsPlaintech/inc/header.php
+++ views/cmsPlaintech/inc/header.php
@@ -1,4 +1,14 @@
     <ul class="nav nav-pills navCM">
+    <?php 
+		$pages = $this->cmstext;
+
+	        $i = 0;
+		    while ($i < count($pages)) {
+		    	echo '<li><a href="/cmsPlaintech/viewPage/'. $pages[$i]["pageid"] .'">'.$pages[$i]["page"].'</a></li>';
+		    	echo"<br/>";
+		    	$i++;
+		    }
+		?>
 	<li><a href="/cmsPlaintech/">Index</a></li>
 	<li><a href="/cmsPlaintech/index">Index</a></li>
 	<li><a href="/cmsPlaintech/manangeVpsParts/Disk">Disk Price</a></li>
--- views/cmsPlaintech/index.php
+++ views/cmsPlaintech/index.php
@@ -3,16 +3,6 @@
 		<?php require 'inc/header.php'; ?>
 	</div>
 	<div class="col-md-7">
-		<?php 
-		$pages = $this->cmstext;
-
-	        $i = 0;
-		    while ($i < count($pages)) {
-		    	echo '<a href="/cmsPlaintech/viewPage/'. $pages[$i]["pageid"] .'">'.$pages[$i]["page"].'</a>';
-		    	echo"<br/>";
-		    	$i++;
-		    }
-		?>
 		<a href="/cmsPlaintech/manangeVpsParts/CPU"><button type="button" class="btn btn-info">CPU</button></a>
 		<a href="/cmsPlaintech/manangeVpsParts/RAM"><button type="button" class="btn btn-info">RAM</button></a>
 		<a href="/cmsPlaintech/manangeVpsParts/Disk"><button type="button" class="btn btn-info">DISK</button></a>
