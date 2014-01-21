e568b9b09dcb3ee8a16391e8b191d62be759a285
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-21 03:15:57 +0100

    management nav

--- views/management/index.php
+++ views/management/index.php
@@ -7,7 +7,7 @@
   </div>
 
   <!-- List group -->
-  <ul class="list-group">
+  <ul class="list-group nav nav-pills nav-stacked">
     <li class="list-group-item">Cras justo odio</li>
     <li class="list-group-item">Dapibus ac facilisis in</li>
     <li class="list-group-item">Morbi leo risus</li>
