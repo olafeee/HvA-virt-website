adf3a582977eef1a574e1d4c144a33605a6bcbf5
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-21 01:58:46 +0100

    Management Page

--- views/klantPaneel/vmselectlist.php
+++
@@ -1,6 +0,0 @@
-             <select id = "myList">
-               <option value = "1">one</option>
-               <option value = "2">two</option>
-               <option value = "3">three</option>
-               <option value = "4">four</option>
-             </select>
