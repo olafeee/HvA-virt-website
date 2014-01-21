92a410f3e76f22be28d6d1ff2ae782feebe9b91b
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-21 17:52:51 +0100

    test

--- views/cmsPlaintech/manangeVpsParts.php
+++ views/cmsPlaintech/manangeVpsParts.php
@@ -12,18 +12,19 @@
 	</thead>
 	<tbody>
 	<?php
+
 		$text = $this->cmsMVP;
 		$kindMVP = $this->kindMVP;
 		
 		$i = 0;
 		while ($i < count($text)) {
 			?>
-			<script type="text/javascript">var xyz = <?php echo '"'.$text[$i][$kindMVP."Amount"].'"'; ?>;</script>
+			<script type="text/javascript">var x<?php echo $i; ?> = <?php echo '"'.$text[$i][$kindMVP."Amount"].'"'; ?>;</script>
 			<?php
 			echo '<tr>';
 			echo '<td>'.$text[$i][$kindMVP."Amount"].'</td>';
 			echo '<td>'.$text[$i][$kindMVP."Price"].'</td>';
-			echo '<td><a href="javascript:showMVP('.$text[$i][$kindMVP[0]."ID"].', xyz, '.$text[$i][$kindMVP."Price"].')" class="sla_ms">more info</a></td>';	
+			echo '<td><a href="javascript:showMVP('.$text[$i][$kindMVP[0]."ID"].', x'.$i.'., '.$text[$i][$kindMVP."Price"].')" class="sla_ms">more info</a></td>';	
 			echo"<tr/>";
 			$i++;
 		}?>
@@ -35,8 +36,8 @@
 	<form name="input" action="/cmsPlaintech/insertMVP" onsubmit="return checkInp()" method="post">
         <input type="hidden" class="pageMVP" name="pageMVP" value="<?php echo $kindMVP; ?>">
         <input type="hidden" class="idMVP" name="idMVP" value="idMVP">
-        <input type="text" class="form-control bfh-number AmountMVP" name="AmountMVP">
-        <input type="text" class="form-control bfh-number PriceMVP" name="PriceMVP" value="PriceMVP">
+		<input type="text" class="form-control bfh-number AmountMVP" name="AmountMVP">';
+		<input type="text" class="form-control bfh-number PriceMVP" name="PriceMVP" value="PriceMVP">
         <div class="faultMVP"></div>
         <button class="btn btn-lg btn-primary btn-block btn-soepmit" type="submit">Sign in</button>
     </form>
