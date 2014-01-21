a6ee4e754df1ccff48b1c2f8ddf27cfdbfb612e7
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-21 12:26:23 +0100

    Clean up + Management pagina

--- views/management/index.php
+++ views/management/index.php
@@ -1,18 +1,26 @@
-<div class="panel panel-info">
-  <div class="panel-heading">
-    <h3 class="panel-title">Panel title</h3>
-  </div>
-  <div class="panel-body">
-    Panel content
-  </div>
+<div class="container">
 
-  <!-- List group -->
-  <ul class="list-group nav nav-pills nav-stacked">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Morbi leo risus</li>
-    <li class="list-group-item">Porta ac consectetur ac</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul>
+  <div class="col-md-3"></div>
 
+    <div class="panel panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">Panel title</h3>
+      </div>
+      <div class="panel-body">
+        Panel content
+      </div>
+
+      <!-- List group -->
+      <ul class="list-group nav nav-pills nav-stacked">
+        <li class="list-group-item"><a href="#">Machine Pannel</a></li>
+        <li class="list-group-item"><a href="#">Account Settings</a></li>
+        <li class="list-group-item"><a href="#">Invoice Overview</a></li>
+        <li class=""><a href="#">Admin Page</a></li>
+      </ul>
+
+    </div>
+  </div>
+  <div class="col-md-9">
+    Test
+  </div>
 </div>
