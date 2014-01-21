4b98ffdc446ca2a865dbcf305ea0eed86b287f6e
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-21 01:14:05 +0100

    lelijk

--- views/account/registerSuccess.php
+++ views/account/registerSuccess.php
@@ -2,8 +2,7 @@
   <div class="col-md-3"></div>
   <div class="col-md-6">
     <div class="login-class">
-      <img src="/img/plaintech-logo.png" id="img-login-logo"><br />
-      
+      <img src="/img/plaintech-logo.png" id="img-login-logo"><br/>   
       <h2>Success!</h2>
       <p>Welcome to PlainTech </p>
       <p>You can start managing your systems in the <a href="/management">Account Panel</a></p>
