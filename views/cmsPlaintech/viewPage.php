d448f96f9f0cdbd0665f4551143f4945e5e15606
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-21 20:53:59 +0100

    place button ipv x

--- views/cmsPlaintech/viewPage.php
+++ views/cmsPlaintech/viewPage.php
@@ -4,22 +4,22 @@
 		<?php require 'inc/header.php'; ?>
 	</div>
 	<div class="col-md-7">
-<?php
+		<?php
 
-	$text = $this->viewpage;
-	print_r($text);
-	$i = 0;
-	while ($i < count($text)) {
-		echo 'number = '.$text[$i]["cwid"];
-		echo"<br/>";
-		echo $text[$i]["cmstext"];
-		echo"<br/>";
-		echo '<a href="/cmsPlaintech/editContent/'.$text[$i]["pageid"].'/'.$text[$i]["cwid"].'">X</a>';
-		echo"<br/>";
-		$i++;
-	}
+			$text = $this->viewpage;
+			//print_r($text);
+			$i = 0;
+			while ($i < count($text)) {
+				echo 'number = '.$text[$i]["cwid"];
+				echo"<br/>";
+				echo $text[$i]["cmstext"];
+				echo"<br/>";
+				echo '<a href="/cmsPlaintech/editContent/'.$text[$i]["pageid"].'/'.$text[$i]["cwid"].'"><button type="button" class="btn btn-info">Edit</button></a>';
+				echo"<br/>";
+				$i++;
+			}
 
-?>
+		?>
 	</div>
 </div>
 	<div class="col-md-3"></div>
