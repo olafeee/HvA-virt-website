97c24c8b1edd5878f7c84de6afcf36daabdadbb4
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-22 20:39:47 +0100

    Fix

--- views/management/accountTab.php
+++ views/management/accountTab.php
@@ -4,16 +4,20 @@
 ** Account Tab on Management Page
 ** ******************************/
 
-
-echo "<pre>";
-print_r($_SESSION);
-
-
-
 ?>
 
 <label for="lname" class="col-sm-2 control-label">Last Name :</label>
 <div class="col-sm-10" id="lname">
 	<?php echo $_SESSION['logArr']['lastname']; ?>
 </div>
+
+
+<br />
+
+<?php 
+
+	echo "<pre>";
+	print_r($_SESSION);
+
+?>
 
