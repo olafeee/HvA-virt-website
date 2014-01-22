b52044a0dac35d9788bee0bf2eb9d1add926ceec
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-22 18:02:15 +0100

    management

--- views/management/index.php
+++ views/management/index.php
@@ -1,6 +1,6 @@
 <div class="row">
 
-  <div class="col-md-3"></div>
+  <div class="col-md-2">
 
     <div class="panel panel-info">
       <div class="panel-heading">
@@ -9,7 +9,6 @@
       <div class="panel-body">
         Panel content
       </div>
-
       <!-- List group -->
       <ul class="list-group nav nav-pills nav-stacked">
         <li class="list-group-item"><a href="#">Machine Pannel</a></li>
@@ -17,19 +16,20 @@
         <li class="list-group-item"><a href="#">Invoice Overview</a></li>
         <li class=""><a href="#">Admin Page</a></li>
       </ul>
-
     </div>
+
   </div>
+
   <div class="col-md-9" role="main">
 
-  <div class="panel panel-info">
-    <div class="panel-heading">
-      <h3 class="panel-title">Panel title</h3>
+    <div class="panel panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">Panel title</h3>
+      </div>
+      <div class="panel-body">
+        <a href="javascript:window.open('/management/console','VM NAME HERE','width=500,height=150')">Open window</a>
+        <br />
+      </div>
     </div>
-    <div class="panel-body">
-      <a href="javascript:window.open('/management/console','VM NAME HERE','width=500,height=150')">Open window</a>
-      <br />
-    </div>
-    
   </div>
 </div>
