a96439a4a949e127c32d88a05c0aec7beb0a81a4
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-22 20:48:22 +0100

    account details

--- views/management/accountTab.php
+++ views/management/accountTab.php
@@ -4,11 +4,38 @@
 ** Account Tab on Management Page
 ** ******************************/
 
+$accountType = $_SESSION['logArr']['lastname'];
+
 ?>
+
+<label for="lname" class="col-sm-2 control-label">First Name :</label>
+<div class="col-sm-10" id="lname">
+	<?php echo $_SESSION['logArr']['firstname']; ?>
+</div>
 
 <label for="lname" class="col-sm-2 control-label">Last Name :</label>
 <div class="col-sm-10" id="lname">
 	<?php echo $_SESSION['logArr']['lastname']; ?>
+</div>
+
+<label for="lname" class="col-sm-2 control-label">Email :</label>
+<div class="col-sm-10" id="lname">
+	<?php echo $_SESSION['logArr']['account']; ?>
+</div>
+
+<label for="lname" class="col-sm-2 control-label">Account Type :</label>
+<div class="col-sm-10" id="lname">
+	<?php
+		if($accountType == '0'){
+			echo "User";
+		} else if($accountType == '2') {
+			echo "Reseller";
+		} else if($accountType == '1') {
+			echo "PlainTech Employ";
+		} else {
+			echo "Unknown";
+		}
+	?>
 </div>
 
 
