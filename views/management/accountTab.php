7ea359b7ef9384a07ae04f383273ed90a2d04537
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-22 20:35:35 +0100

    Layout account info

--- views/management/accountTab.php
+++ views/management/accountTab.php
@@ -4,9 +4,18 @@
 ** Account Tab on Management Page
 ** ******************************/
 
+$accountDetials = 
+
+
 echo "<pre>";
 print_r($_SESSION);
 
+
+
 ?>
 
+<label for="lname" class="col-sm-2 control-label">Last Name :</label>
+<div class="col-sm-10" id="lname">
+	<?php echo $_SESSION['logArr']['lastname']; ?>
+</div>
 
