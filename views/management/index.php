9fe7d9c1df4ca66211dfe968ff8abcba6cf2c468
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-22 18:08:24 +0100

    management

--- views/management/index.php
+++ views/management/index.php
@@ -1,21 +1,21 @@
 <div class="row">
 
-  <div class="col-md-2">
+  <div class="col-md-3">
 
     <div class="panel panel-info">
       <div class="panel-heading">
         <h3 class="panel-title">Panel title</h3>
       </div>
+      <div class="list-group">
+        <a href="#" class="list-group-item active">Machine Pannel</a>
+        <a href="#" class="list-group-item">Account Settings</a>
+        <a href="#" class="list-group-item">Invoice Overview</a>
+        <a href="#" class="list-group-item">Admin Page</a>
+      </div>
       <div class="panel-body">
         Panel content
       </div>
-      <!-- List group -->
-      <ul class="list-group nav nav-pills nav-stacked">
-        <li class="list-group-item"><a href="#">Machine Pannel</a></li>
-        <li class="list-group-item"><a href="#">Account Settings</a></li>
-        <li class="list-group-item"><a href="#">Invoice Overview</a></li>
-        <li class=""><a href="#">Admin Page</a></li>
-      </ul>
+
     </div>
 
   </div>
