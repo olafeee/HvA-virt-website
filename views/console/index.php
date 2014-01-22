9c7febab67234b40ca7ff694c00d8e94f44c3e05
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-22 01:00:34 +0100

    Made ajax post

--- views/console/index.php
+++ views/console/index.php
@@ -23,6 +23,7 @@
         $( document ).ready(function() {
             window.open(logonurl, "logon");
             $.ajax({
+                type: "POST",
                 url: logonurl,
                 context: document.body
             }).done(function() {
