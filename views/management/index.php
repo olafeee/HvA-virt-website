9fc9057a2fba99705097374b04eda71e7f60270f
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-22 18:21:30 +0100

    management

--- views/management/index.php
+++ views/management/index.php
@@ -1,3 +1,10 @@
+<script>
+  $(function() {
+    $( "#tabs" ).tabs();
+  });
+</script>
+
+
 <div class="row">
 
   <div class="col-md-3">
@@ -7,27 +14,64 @@
         <h3 class="panel-title">Panel title</h3>
       </div>
       <div class="list-group">
-        <a href="#" class="list-group-item">Machine Pannel</a>
-        <a href="#" class="list-group-item">Account Settings</a>
-        <a href="#" class="list-group-item">Invoice Overview</a>
+        <a href="#tabs-1" class="list-group-item">Machine Pannel</a>
+        <a href="#tabs-2" class="list-group-item">Account Settings</a>
+        <a href="#tabs-3" class="list-group-item">Invoice Overview</a>
         <a href="#" class="list-group-item">Admin Page</a>
       </div>
     </div>
 
   </div> <!-- END Col 3 -->
+  <div id="tabs">
 
-  <div class="col-md-9" role="main">
+    <div id="tabs-1">
 
-    <div class="panel panel-info">
-      <div class="panel-heading">
-        <h3 class="panel-title">Panel title</h3>
-      </div>
-      <div class="panel-body">
-        <a href="javascript:window.open('/management/console','VM NAME HERE','width=500,height=150')">Open window</a>
-        <br />
-      </div>
+      <div class="col-md-9" role="main">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel title</h3>
+          </div>
+          <div class="panel-body">
+            <a href="javascript:window.open('/management/console','VM NAME HERE','width=800,height=500')">Open window 1111111111</a>
+            <br />
+          </div>
+        </div>
+      </div><!-- END Col 9 -->
+
     </div>
 
-  </div><!-- END Col 9 -->
+    <div id="tabs-2">
+
+      <div class="col-md-9" role="main">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel title</h3>
+          </div>
+          <div class="panel-body">
+            <a href="javascript:window.open('/management/console','VM NAME HERE','width=800,height=500')">Open window 22222222222222 </a>
+            <br />
+          </div>
+        </div>
+      </div><!-- END Col 9 -->
+      
+    </div>
+
+    <div id="tabs-3">
+
+      <div class="col-md-9" role="main">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel title</h3>
+          </div>
+          <div class="panel-body">
+            <a href="javascript:window.open('/management/console','VM NAME HERE','width=800,height=500')">Open window333333333333333333</a>
+            <br />
+          </div>
+        </div>
+      </div><!-- END Col 9 -->
+      
+    </div>
+
+  </div>
 
 </div>
