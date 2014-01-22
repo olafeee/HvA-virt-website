8d700ac71e764c631cf9bf2cfcccbf1ea290ac40
Dinesh497 <dinesh.bhagwandin@hva.nl>
2014-01-22 16:47:57 +0100

    test

--- controllers/shoppingbasket.php
+++ controllers/shoppingbasket.php
@@ -15,15 +15,16 @@ class Shoppingbasket extends baseController {
 	*/
 	function createVM() {
 		if (Session::get('loggedIn') == true) {
-			$serviceofferinid="eaacfa01-6e2f-4a5a-a789-03f259c8a644";
+			$serviceofferingid ="eaacfa01-6e2f-4a5a-a789-03f259c8a644";
 			$templateid="7fd68000-5783-47f0-b0f1-4bae45946b4e";
 			$zoneid="bc1354a3-58b4-4f98-ab51-7d4406260e15";
 			$diskofferingid= null;
 			$displayname= "centos6.5";
 			$name="centos6.5minimal";
-			$account=$_SESSION['logArr']['userid'];
-			$domainid=$_SESSION['logArr']['domainid'];
-
+			$account= null;
+			$domainid= null;
+			//$_SESSION['logArr']['userid']
+			//$_SESSION['logArr']['domainid']
 			$this->cloudstack->deployVirtualMachine($serviceofferingid, $templateid, $zoneid, $diskofferingid, $displayname, $name, $account, $domainid );
 
 		}else{
