2780ffe63ed9b3109512fb02c634d7650ccba20f
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-22 20:54:42 +0100

    what does this do?

--- views/management/accountTab.php
+++ views/management/accountTab.php
@@ -4,28 +4,28 @@
 ** Account Tab on Management Page
 ** ******************************/
 
-$accountType = $_SESSION['logArr']['lastname'];
-
 ?>
 
 <label for="fname" class="col-sm-2 control-label">First Name :</label>
-<div class="col-sm-10" id="fname">
-	<?php echo $_SESSION['logArr']['firstname']; ?><br />
+<div class="col-sm-10">
+	<p id="fname"><?php echo $_SESSION['logArr']['firstname']; ?></p>
 </div>
 
 <label for="lname" class="col-sm-2 control-label">Last Name :</label>
 <div class="col-sm-10" id="lname">
-	<?php echo $_SESSION['logArr']['lastname']; ?>
+	<?php echo $_SESSION['logArr']['lastname']; ?><br />
 </div>
 
 <label for="email" class="col-sm-2 control-label">Email :</label>
 <div class="col-sm-10" id="email">
-	<?php echo $_SESSION['logArr']['account']; ?>
+	<?php echo $_SESSION['logArr']['account']; ?><br />
 </div>
 
 <label for="type" class="col-sm-2 control-label">Account Type :</label>
 <div class="col-sm-10" id="type">
 	<?php
+		$accountType = $_SESSION['logArr']['lastname'];
+
 		if($accountType == '0'){
 			echo "User";
 		} else if($accountType == '2') {
@@ -35,7 +35,7 @@ $accountType = $_SESSION['logArr']['lastname'];
 		} else {
 			echo "Unknown";
 		}
-	?>
+	?><br />
 </div>
 
 <br />
