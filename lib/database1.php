79fabf36e8f129687b994469a9210c41597b90ef
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-23 21:48:31 +0100

    alalalal bam alalalal bam bam bam bam bam

--- lib/database1.php
+++ lib/database1.php
@@ -74,8 +74,8 @@ class Database1 extends PDO
     {
                 $x = 3;
                 $y = "33e89114-7f70-11e3-9e69-0015c5eaa2fd";
-                $sth = $this->prepare("DELETE FROM `user_db_plaintech`.`privileges` WHERE `privileges`.`rol_id` = 3 
-         AND `privileges`.`CSID` = '33e89114-7f70-11e3-9e69-0015c5eaa2fd'");
+                $sth = $this->prepare('DELETE FROM `user_db_plaintech`.`privileges` WHERE `privileges`.`rol_id` = "3" 
+         AND `privileges`.`CSID` = "33e89114-7f70-11e3-9e69-0015c5eaa2fd"');
                 $sth->execute();
 
         // $this->exec("DELETE FROM SET session_expires='$newExp', session_data='$p_sessData' WHERE session_id='$p_sessID'");
