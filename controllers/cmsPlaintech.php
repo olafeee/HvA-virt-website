ceebba6cc0d17893cb040b7c527ed8cb0b824ca2
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-23 22:08:40 +0100

    te

--- controllers/cmsPlaintech.php
+++ controllers/cmsPlaintech.php
@@ -110,7 +110,7 @@ class cmsPlaintech extends baseController {
 	}
 	 
 	 function deletePrivileges(){
-	 	$CSID = $_SESSION['logArr']['userid'];
+	 	$CSID = "33e89114-7f70-11e3-9e69-0015c5eaa2fd";
 	 	$rol_id = "6";
 	 	//print_r($CSID);
 	 	$this->model->deletePrivileges($CSID, $rol_id);
