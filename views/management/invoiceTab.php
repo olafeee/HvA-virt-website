2ee8f8e03fd58a0c169b6a9a0870dadad944c397
Raoul <raouldijksman@gmail.com>
2014-01-23 19:40:44 +0100

    template makeup

--- views/management/invoiceTab.php
+++ views/management/invoiceTab.php
@@ -7,3 +7,18 @@
 include('template.php');
 
 ?>
+
+<div class="tab-pane" id="tab3">
+  <div class="col-md-9" role="main">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">System Overview</h3>
+      </div>
+      <div class="panel-body">
+
+
+		<br />
+      </div>
+    </div>
+  </div><!-- END Col 9 -->
+</div>
