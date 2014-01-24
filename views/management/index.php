4d70733295cffad2484184776bc3beb58d039ade
Raoul <raouldijksman@gmail.com>
2014-01-24 12:11:01 +0100

    Makeup 2?

--- views/management/index.php
+++ views/management/index.php
@@ -14,39 +14,34 @@ $vmResponce;
       <div class="panel-body">
         
         <!-- Start VM listing here -->
-         <table class="table">
+        <table class="table">
         <thead>
-        <tr>
-        <th>VM</th>
-        <th>Status</th>
-        <th>CPU</th>
-        <th>CPU speed</th>
-        <th>Memory</th>
-        <th>HDD</th>
-        <th>IP Adres</th>
-        <th></th>
-        </tr>
+          <tr>
+            <th>VM</th>
+            <th>IP Address</th>
+            <th>Status</th>
+          </tr>
         </thead>
-        <tbody><?php
-                    $xy = 0;
-                    $maxArray = count($vmResponce);
-                    while ($xy < $maxArray) {?>
-        <tr class="success">
-        <td class"displayname"><?php echo $vmResponce[$xy]["displayname"];?></td>
-        <td class="status"><?php echo $vmResponce[$xy]["state"];?></td>
-        <td class="CPU"><?php echo $vmResponce[$xy]["cpunumber"];?></td>
-        <td class="CPUSPEED"><?php echo $vmResponce[$xy]["cpuspeed"];?> Mhz</td>
-        <td class="memory"><?php echo $vmResponce[$xy]["memory"];?> MB</td>
-        <td class="HHD">20GB</td>
-
-        <td class="IPAdres"><?php echo $vmResponce[$xy]["nic"][0]["ipaddress"]." /". prefixSubnet($vmResponce[$xy]["nic"][0]["netmask"]);?></td>
-        <td><a href="/management/vminfo/<?php echo $xy; ?>"><button type="button" class="btn btn-info"> Info </button></a> <button type="button" class="btn btn-success"> Upgrade </button></td>
-        </tr><?
-                      $xy++;
-                    }
-              ?></tbody>
+        <tbody>
+          <?php
+            $i = 0;
+            $maxArray = count($vmResponce);
+            while ($i < $maxArray) { 
+          ?>
+              <a href="/management/vminfo/<?php echo $xy; ?>">
+                <tr id="<?php echo $vmResponce[$i]['id']; ?>" class="success">
+                  <td class"displayname"><?php echo $vmResponce[$i]['displayname'];?></td>
+                  <td class="IPAdres"><?php echo $vmResponce[$i]['nic'][0]['ipaddress']." /". prefixSubnet($vmResponce[$xy]["nic"][0]["netmask"]);?></td>
+                  <td class="status"><?php echo $vmResponce[$i]['state'];?></td>
+                  <td></td>
+                </tr>
+              </a>
+          <?php
+              $i++;
+            } // End While loop
+          ?>
+        </tbody>
         </table>
-
         
         <br />
       </div>
