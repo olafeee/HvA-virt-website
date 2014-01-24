33e16b75f4f8ebd1af8b80c05a263f4e695676b0
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-24 21:58:06 +0100

    test

--- models/cmsPlaintechModel.php
+++ models/cmsPlaintechModel.php
@@ -100,7 +100,7 @@ class cmsPlaintechModel extends baseModel
 	        return $x;    
 	}
 
-	public function addPrivileges()
+	public function addPrivileges($role_id, $CSID)
     {
         $x = $this->db->insertRole($role_id, $CSID);
         return $x;
