7e48d1e166365bf4b6318e7f7cb6eda32c44d723
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-24 20:02:19 +0100

    e

--- views/management/index.php
+++ views/management/index.php
@@ -12,13 +12,12 @@ $vmResponse = $this->vmResponse;
     $subBin = explode( '.', $input );
     $subBinX = 0;
     $subnet = 0;
-    print_r($input);
-    /*while ($subBinX <= 3) {
+    while ($subBinX <= 3) {
       $x = decbin($subBin[$subBinX]);
       $var3 = strlen(str_replace('0', '', $x));
       $subnet = $subnet + $var3;
       $subBinX++;
-    }*/
+    }
     return $subnet;
   }
 ?>
