d758307baea4e6dba32a4166718ddd8b80de3f4c
Raoul <raouldijksman@gmail.com>
2014-01-24 13:27:24 +0100

    hoe ziet dit er uit

--- views/management/index.php
+++ views/management/index.php
@@ -35,8 +35,65 @@ $vmResponse = $this->vmResponse;
             $maxArray = count($vmResponse['virtualmachine']);
             while ($i < $maxArray) { 
           ?>
-              <a href="/management/vminfo/<?php echo $xy; ?>">
-                <tr id="<?php echo $vmResponse[$i]['id']; ?>" class="success">
+              <a href="/management/vminfo/<?php echo $i; ?>">
+                <tr id="<?php echo $vmResponse[$i]['id']; ?>" class="success alert-success">
+                  <td class"displayname"><?php echo $vmResponse['virtualmachine'][$i]['displayname'];?></td>
+                  <td class="IPAdres"><?php echo $vmResponse['virtualmachine'][$i]['nic'][0]['ipaddress']." /". ($vmResponse['virtualmachine'][$i]["nic"][0]["netmask"]);?></td>
+                  <td class="status"><?php echo $vmResponse['virtualmachine'][$i]['state'];?></td>
+                  <td></td>
+                </tr>
+              </a>
+          <?php
+              $i++;
+            } // End While loop
+          ?>
+        </tbody>
+        </table>
+        
+        <br />
+      </div>
+    </div>
+  </div><!-- END Col 9 -->
+</div>
+<?php 
+include('template.php'); 
+
+$vmResponse = $this->vmResponse;
+?>
+
+
+<div class="tab-pane" id="tab3">
+  <div class="col-md-9" role="main">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">System Overview</h3>
+      </div>
+      <div class="panel-body">
+        
+        <?php
+          // Check if user has any Vms
+
+        ?>
+
+
+        <!-- Start VM listing here -->
+        <table class="table">
+        <thead>
+          <tr>
+            <th>VM</th>
+            <th>IP Address</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            $i = 0;
+            print_r($vmResponse);
+            $maxArray = count($vmResponse['virtualmachine']);
+            while ($i < $maxArray) { 
+          ?>
+              <a href="/management/vminfo/<?php echo $i; ?>">
+                <tr id="<?php echo $vmResponse[$i]['id']; ?>" class="success alert-success">
                   <td class"displayname"><?php echo $vmResponse['virtualmachine'][$i]['displayname'];?></td>
                   <td class="IPAdres"><?php echo $vmResponse['virtualmachine'][$i]['nic'][0]['ipaddress']." /". ($vmResponse['virtualmachine'][$i]["nic"][0]["netmask"]);?></td>
                   <td class="status"><?php echo $vmResponse['virtualmachine'][$i]['state'];?></td>
