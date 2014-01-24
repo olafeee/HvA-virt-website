eb713b33efc6ede21c762f483f7132e45ea33d34
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-24 20:23:13 +0100

    te

--- lib/baseController.php
+++ lib/baseController.php
@@ -5,12 +5,13 @@ class baseController {
 		$this->baseView = new baseView();
 		$url = $this->urlfix();
 		$this->baseView->url0 = $url[0];
+
 		#################################################
 		# check of de persoon recht heeft	#yolo		#
 		# laad hierboven nog wel conDB1() in!!!!!!!!!!!!#
 		#################################################
 		$url = $this->urlfix();
-		$getRole = $this->model->getRoleByPage($url[0]);
+		$getRole = $this->model->getRoleByPage();
 		$role = $this->in_array_r($getRole);
 		if($role == false){
 			header('location: /account');
