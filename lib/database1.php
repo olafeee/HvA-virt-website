ab8d5aa7214f5b29d91c208e51333c7c4ad43f4c
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-25 21:45:56 +0100

    test

--- lib/database1.php
+++ lib/database1.php
@@ -89,11 +89,12 @@ class Database1 extends PDO
     public function jan(){
         $this->beginTransaction();
         $sql = "
-    INSERT INTO `user_db_plaintech`.`privileges` (`rol_id`, `CSID`) VALUES ('3', '1150da1b-6580-4321-954a-47ef7fc09372');
-    INSERT INTO `user_db_plaintech`.`users` (`id`, `login`,`password`) VALUES ('3', 'jan', 'kaas');
-";
+        INSERT INTO `user_db_plaintech`.`privileges` (`rol_id`, `CSID`) VALUES ('3', '1150da1b-6580-4321-954a-47ef7fc09372');
+        INSERT INTO `user_db_plaintech`.`users` (`id`, `login`,`password`) VALUES ('3', 'jan', 'kaas');
+        ";
+
         $sth = $this->prepare($sql);
-        $this->commit();  
+        $sth->commit();  
         return "het werkt of niet";
     }
 
