cfecb14c8fe0ab8a6333ea16115e7846a6a90e84
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-25 21:56:00 +0100

    test

--- lib/database1.php
+++ lib/database1.php
@@ -90,9 +90,7 @@ class Database1 extends PDO
         $this->beginTransaction();
         $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sth = $this->prepare(" INSERT INTO `user_db_plaintech`.`privileges` (`rol_id`, `CSID`) 
-                                VALUES ('3', '1150da1b-6580-4321-954a-47ef7fc09372');
-                                    INSERT INTO `user_db_plaintech`.`users` (`id`, `login`,`password`) 
-                                    VALUES ('3', 'jan', 'kaas');");
+                                VALUES ('3', '1150da1b-6580-4321-954a-47ef7fc09372')");
         $sth->execute();  
         $this->commit();
         return "het werkt of niet";
