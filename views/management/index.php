d1ba33a0fb39b52d7e2db020f2f5a018605cdeb6
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-25 07:49:47 +0100

    cleanup

--- views/management/index.php
+++ views/management/index.php
@@ -30,6 +30,7 @@
 
 
 <script type="text/javascript">
+
 // Ajax Auto reload!
 var url;
 var refresh;
@@ -69,90 +70,5 @@ $(document).ready(function() {
   } 
 });
 
-// Popups!
-$(document).ready(function() {
-  // $('head').append('<script type="text/javascript" src="script.js"><%2Fscript>');
-  // $('head').append('<link href="style/style.css" rel="popupStyle.css" type="text/css" media="all" />');
-});
 
 </script>
-
-
-
-
-
-
-
-
-
-
-
-
-<?php /*
-<div class="tab-pane" id="tab3">
-  <div class="col-md-9" role="main">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h3 class="panel-title">System Overview</h3>
-      </div>
-      <div class="panel-body">
-        
-        <?php
-          // Check if user has any Vms
-
-        ?>
-
-
-        <!-- Start VM listing here -->
-        <table class="table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Address</th>
-            <th>CPU</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-            $i = 0;
-            //print_r($vmResponse);
-            $maxArray = count($vmResponse['virtualmachine']);
-            while ($i < $maxArray) { 
-              if($vmResponse['virtualmachine'][$i]['state'] == 'Running' ) {
-                $stateColor = 'alert-success';
-              } else if($vmResponse['virtualmachine'][$i]['state'] == 'Stopped' ) {
-                $stateColor = 'alert-danger';
-              } else {
-                $stateColor = '';
-              }
-          ?>
-              
-              <tr id="<?php echo $vmResponse[$i]['id']; ?>" class="success <?php echo $stateColor;?>" onclick="document.location= '/management/vminfo/<?php echo $vmResponse['virtualmachine'][$i]['id']; ?>';">
-                <td class"displayname"><?php echo $vmResponse['virtualmachine'][$i]['displayname'];?></td>
-                <td class="CPU"><?php echo $vmResponse['virtualmachine'][$i]["cpunumber"];?></td>
-                <td class="CPUSPEED"><?php echo $vmResponse['virtualmachine'][$i]["cpuspeed"];?> Mhz</td>
-                <td class="memory"><?php echo $vmResponse['virtualmachine'][$i]["memory"];?> MB</td>
-                <td class="HHD">20GB</td>
-                <td class="IPAdres"><?php echo $vmResponse['virtualmachine'][$i]['nic'][0]['ipaddress']." / ". prefixSubnet($vmResponse['virtualmachine'][$i]["nic"][0]["netmask"]);?></td>
-                <td class="status"><?php echo $vmResponse['virtualmachine'][$i]['state'];?></td>
-              </tr>
-          <?php
-              $i++;
-            } // End While loop
-          ?>
-        </tbody>
-        </table>
-        
-        <br />
-      </div>
-    </div>
-  </div><!-- END Col 9 -->
-</div>
-
-<script type="text/javascript">
-  // Zorg er voor dat de pagina om de paar seconde een refresh doet
-  setTimeout(function () { location.reload(1); }, 6000);
-</script>
-
-*/ ?>
