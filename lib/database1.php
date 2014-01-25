d5bfe98e06b39f9e8450eaf83d003843cb3a17ba
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-25 21:59:35 +0100

    test

--- lib/database1.php
+++ lib/database1.php
@@ -92,7 +92,7 @@ class Database1 extends PDO
         $sth = $this->prepare(" INSERT INTO `user_db_plaintech`.`privileges` (`rol_id`, `CSID`) 
                                 VALUES ('3', '1150da1b-6580-4321-954a-47ef7fc09372')");
 
-           $sth = $this->prepare(  "                    INSERT INTO `user_db_plaintech`.`users` (`id`, `login`,`password`) 
+           $sth = $this->prepare("INSERT INTO `user_db_plaintech`.`users` (`id`, `login`,`password`) 
                                     VALUES ('3', 'jan', 'kaas')");
         $sth->execute();  
         $this->commit();
