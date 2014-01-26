f801a8944df9ce414d91768ade92eb73bd9dfb9e
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-26 18:38:50 +0100

    self is weg?

--- models/shoppingbasketModel.php
+++ models/shoppingbasketModel.php
@@ -39,7 +39,7 @@ class shoppingbasketModel extends baseModel
 
 		session_start();
 		// Initialize variables
-		require('./lib/invoice/invoice.php');
+		require('/lib/invoice/invoice.php');
 
 		if (empty($_SESSION['cart'])){
 			if(header('location /order')){
@@ -207,14 +207,16 @@ class shoppingbasketModel extends baseModel
 				$pdfname = uniqid('invoice_').".pdf";
 				$pdf->Output('/var/invoices/'.$pdfname, 'F');
 				//self::sentInvoice("a@b.c", "/tmp/1.pdf");
-				self::saveToDb($pdfname, $klantId, $klantFNaam, $klantLNaam);
+				//self::saveToDb($pdfname, $klantId, $klantFNaam, $klantLNaam);
+				saveToDb($pdfname, $klantId, $klantFNaam, $klantLNaam);
 				$_SESSION['invoice'] = "invoiceSaved";
 			}
 		}
 		
 		$attachment = $pdf->Output('', 'S');
 		//self::sentInvoice("robert.van.lierop@hva.nl", $attachment);
-		self::sentInvoice($klantId, $attachment);
+		//self::sentInvoice($klantId, $attachment);
+		sentInvoice($klantId, $attachment);
 	}
 	
 	function saveToDb($filename, $userid, $fsname, $lsname){
