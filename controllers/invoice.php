10814a05bf2aa696b2cbdcb2d7aa87c5feb7b0e5
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-26 16:50:45 +0100

    s

--- controllers/invoice.php
+++ controllers/invoice.php
@@ -28,7 +28,7 @@ class invoice extends baseController {
 		$invoice = $this->model->getAll($limit);
 		$this->baseView->invoices = $invoice;
 		//print_r($invoice);
-		$this->index('invoice/index');
+		$this->index('index', TRUE);
 	}
 	function sortOnTime($limit){
 		$invoice = $this->model->getOnTime($limit);
