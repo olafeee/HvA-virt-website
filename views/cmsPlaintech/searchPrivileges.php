beeeb904b20138a65b7c4e27bdbf57819367687c
oscarvlugt <oscarvlugt@hotmail.com>
2014-01-26 23:07:31 +0100

    t

--- views/cmsPlaintech/searchPrivileges.php
+++ views/cmsPlaintech/searchPrivileges.php
@@ -28,6 +28,8 @@
 			$i = 0;
 			while ($i < count($nameFound)) {
 				// echo $nameFound[$i]['username'];
+				echo '<td>'.$nameFound[$i]['firstname'].'</td>';
+				echo '<td>'.$nameFound[$i]['lastname'].'</td>';
 				echo '<td>'.$nameFound[$i]['username'].'</td>';
 				// echo '<a href="/cmsPlaintech/managePrivileges/'.$nameFound[$i]['CSID'].'/'.$nameFound[$i]['username'].'">Bekijk</a>';
 				// echo '</br>';
