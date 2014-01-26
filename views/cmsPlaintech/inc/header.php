f9d6e11fe30c57b4aa41d25b159188685cb967de
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-26 21:32:57 +0100

    sav

--- views/cmsPlaintech/inc/header.php
+++ views/cmsPlaintech/inc/header.php
@@ -1,6 +1,4 @@
-    <?php $pages = $this->cmstext;
-print_r($pages);
-    ?>
+    <?php $pages = $this->cmstext; ?>
 
 
 
@@ -13,18 +11,19 @@ print_r($pages);
         <h3 class="panel-title">Management Panel</h3>
       </div>
       <div class="list-group">
-      	<?php
-      		$i = 0;
-		    while ($i < count($pages)) {
-
-		    	echo '<a href="/cmsPlaintech/viewPage/'. $pages[$i]["pageid"] .'" class="list-group-item">'.$pages[$i]["page"].'</a>';
-		    	$i++;
-		    }
-      	?>
-      	<a href="/cmsPlaintech/manangeVpsParts/CPU" class="list-group-item">CPU Price</a>
-		<a href="/cmsPlaintech/manangeVpsParts/RAM" class="list-group-item">RAM Price</a>
-		<a href="/cmsPlaintech/manangeVpsParts/Disk" class="list-group-item">Disk Price</a>
         <a href="/cmsPlaintech/searchPrivileges" class="list-group-item">Search Privileges</a>
+      	<a href="/cmsPlaintech/manangeVpsParts/CPU" class="list-group-item">CPU Price</a>
+		  <a href="/cmsPlaintech/manangeVpsParts/RAM" class="list-group-item">RAM Price</a>
+		  <a href="/cmsPlaintech/manangeVpsParts/Disk" class="list-group-item">Disk Price</a>
+        
+                <?php
+          $i = 0;
+        while ($i < count($pages)) {
+
+          echo '<a href="/cmsPlaintech/viewPage/'. $pages[$i]["pageid"] .'" class="list-group-item">'.$pages[$i]["page"].'</a>';
+          $i++;
+        }
+        ?>
       </div>
     </div>
 
