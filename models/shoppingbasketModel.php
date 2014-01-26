4a7ccaac5b32c1e8920a24bb5db423eccea9ea87
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-26 18:49:58 +0100

    wtf

--- models/shoppingbasketModel.php
+++ models/shoppingbasketModel.php
@@ -37,9 +37,8 @@ class shoppingbasketModel extends baseModel
 		ini_set('display_errors', 1);
 
 
-		session_start();
 		// Initialize variables
-		require('/lib/invoice/invoice.php');
+		require('./lib/invoice/invoice.php');
 
 		if (empty($_SESSION['cart'])){
 			if(header('location /order')){
