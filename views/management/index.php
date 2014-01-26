a58c2412fdb44aba6fc71b807b7830f124c8d616
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-26 19:25:27 +0100

    dange

--- views/management/index.php
+++ views/management/index.php
@@ -2,7 +2,7 @@
 ?>
 
 <div class="Warningdiv">
-  <div class="panel panel-default">
+  <div class="panel panel-danger">
     <div class="panel-heading">
       <h3 class="panel-title">Warning!</h3>
     </div>
