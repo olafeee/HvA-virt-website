4d3c8c44690b3c52f648aab74afe950d40638ffe
Raoul <Raoul.Dijksman.Mensenkamp@hva.nl>
2014-01-26 14:40:18 +0100

    test

--- views/management/vminfo.php
+++ views/management/vminfo.php
@@ -156,7 +156,7 @@ else
     <a type="button"  class="btn btn-primary" href="/management/console/<?php echo $vmResponse[0]['id']; ?>" style="margin-bottom:5px; width:125px;" onclick="window.open(this.href, 'Console', 'width=735, height=540, left=24, top=24, scrollbars, resizable'); return false;">View Console</a>
   	<button type="button" class="btn btn-primary sendCmdButton disabled" id="upgrade" style="margin-bottom:5px; width:125px;">Upgrade System !!!</button>
     <button type="button" class="btn btn-primary sendCmdButton disabled" id="backup" style="margin-bottom:5px; width:125px;">Backup System</button>
-    <button type="button" class="btn btn-danger popupCmdButton" id="destroy" style="margin-bottom:5px; width:125px;"><span class="glyphicon glyphicon-exclamation-sign"></span>  Destroy System</button>
+    <button type="button" class="btn btn-danger popupCmdButton" id="destroy" style="margin-bottom:5px; min-width:125px;"><span class="glyphicon glyphicon-exclamation-sign"></span>  Destroy System</button>
 
     <br />
   </div>
@@ -189,6 +189,7 @@ else
     var id = $(this).attr('id');
     var name = '#popup';
     name = name.concat(id); 
+    console.log(name);
     //change display to block
     $(name).css("display", "block");
     $(".opacitySLAdiv").css("display", "block");
