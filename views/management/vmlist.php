cc511b71dcaa874541ce57717721872091f1e5b7
OT Elzinga <olaf.elzinga@hva.nl>
2014-01-27 09:07:12 +0100

    te

--- views/management/vmlist.php
+++ views/management/vmlist.php
@@ -58,9 +58,9 @@ $vmResponse = $this->vmResponse;
         $maxArray = count($vmResponse['virtualmachine']);
         while ($i < $maxArray) {
 
-          if ($vmResponse['virtualmachine'][$i]['state'] != 'Expunging' || $vmResponse['virtualmachine'][$i]['state'] != 'Error') {
-             
-             if($vmResponse['virtualmachine'][$i]['state'] == 'Running' ) {
+          if ($vmResponse['virtualmachine'][$i]['state'] != 'Expunging') {
+            if ($vmResponse['virtualmachine'][$i]['state'] != 'Error') {
+                           if($vmResponse['virtualmachine'][$i]['state'] == 'Running' ) {
               $stateColor = 'alert-success';
             } else if($vmResponse['virtualmachine'][$i]['state'] == 'Stopped' ) {
               $stateColor = 'alert-danger';
@@ -81,6 +81,9 @@ $vmResponse = $this->vmResponse;
             </tr>
         <?php
            
+            }
+             
+
           }
           $i++;
         } // End While loop
