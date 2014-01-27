f12e3e5e0bf5728d7467ea737444389d1a6349ae
oscarvlugt <oscarvlugt@hotmail.com>
2014-01-27 01:16:25 +0100

    Text ingevuld

--- views/cmsPlaintech/index.php
+++ views/cmsPlaintech/index.php
@@ -14,10 +14,23 @@
       <p>
         This page is specificly designed for employees and/or managers of Plaintech. Through this page, they can manage the contents of this site.
       </p>
-      <h2>CPU Price</h2>
+      <h4>CPU Price</h4>
       <p>
         Here you can modify the price per CPU core or physical CPU. You can even add special prices to a specific ammount of cores.
       </p>
+       <h4>RAM Price</h4>
+      <p>
+        Here you can modify the price for each unit of RAM that is assigned to a VPS. 
+      </p>
+      <h4>Disk Price</h4>
+      <p>
+        This page allows you to select a price for specific storage space units that is available for a VPS.
+      </p>
+      <h4>Index</h4>
+      <p>
+        The index is your main page. Your presentation towards your customers. You can change the text here that defines your company.
+      </p>
+      <h4>More to Come</h4>
 	</div>
       </div>
     </div>
